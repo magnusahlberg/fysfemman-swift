a6e7c614e2c4b6ea0c8d0dc2e8b79f929248bf2a
Magnus Ahlberg <m@magnus.xyz>
2017-02-13 10:58:52 +0100

    Basic authentication and templating foundation

---
+++ Package.swift
@@ -0,0 +1,11 @@
+import PackageDescription
+
+let package = Package(
+    name: "Fysfemman",
+    dependencies: [
+         .Package(url: "https://github.com/IBM-Swift/Kitura.git",                       majorVersion: 1),
+         .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1),
+         .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git",               majorVersion: 1),
+         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",                 majorVersion: 1)
+    ]
+)
