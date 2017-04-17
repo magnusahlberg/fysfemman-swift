32cdeaa28a1aeabd729b5fb367440534f9496586
Magnus Ahlberg <m@magnus.xyz>
2017-04-18 00:15:49 +0200

    Remove Stencil from dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "Fysfemman",
     dependencies: [
          .Package(url: "https://github.com/IBM-Swift/Kitura.git",                       majorVersion: 1),
-         .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1),
          .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git",               majorVersion: 1),
          .Package(url: "https://github.com/IBM-Swift/Kitura-CORS.git",                  majorVersion: 1),
          .Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL",           majorVersion: 0),
